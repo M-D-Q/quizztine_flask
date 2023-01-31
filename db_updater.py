f83fef038d13e8fb0e0f2ae73ff0743115da4964
unknown <romain.jacek@gmail.com>
2023-01-31 11:59:32 +0100

    change question id 112

--- db_updater.py
+++ db_updater.py
@@ -29,11 +29,25 @@ def mod_explanation (app, db):
       db.session.add(question_a_changer)
       db.session.commit()
 
+def mod_question (app, db):
+   with app.app_context():
+      input_id = int(input("""Which ID:
+   --> """))
+      question_a_changer = db.session.query(QuestionsHTML).filter_by(id=input_id).first()
+      print(f"Current question : {question_a_changer.question_html}")
+      questionhtml_change = input("Your change: ")
+      question_a_changer.question_html = questionhtml_change
+      db.session.add(question_a_changer)
+      db.session.commit()
 
-choice = int(input("Modify 1.Answer or 2.Explanation ?"))
+
+
+choice = int(input("Modify 1.Answer or 2.Explanation or 3.Question ?"))
 if choice == 1 :
    mod_answer(app,db)
 elif choice == 2 :
    mod_explanation(app,db)
+elif choice == 3 :
+   mod_question(app,db)
 else :
    print("Miaou")
