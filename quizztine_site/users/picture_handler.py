96441bb6b2c4426447a082ee89a55d6f479598db
RomJHD <romain.jacek@gmail.com>
2023-05-30 11:24:31 +0200

    update profile pic

--- quizztine_site/users/picture_handler.py
+++ quizztine_site/users/picture_handler.py
@@ -7,10 +7,11 @@ def add_profile_pic(pic_upload, username):
     ext_type = filename.split('.')[-1]
     storage_filename = str(username)+'.'+ext_type
 
-    filepath = os.path.join(current_app.root_path, 'static\profile_pics', storage_filename)
+    filepath = os.path.join(current_app.root_path, 'static/profile_pics', storage_filename)
     output_size = (200,200)
 
     pic = Image.open(pic_upload)
     pic.thumbnail(output_size)
     pic.save(filepath)
+
     return storage_filename
